90a549feba48e9c7e97c00afb1e4c0de891f6fe0
Lawrence Maloney <lawrence.maloney@att.net>
2022-06-25 17:39:07 -0500

    One more change

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Building app..."
 npm run build 
 
 echo "Deploying files to server..."
-scp -r build/* larrymm@173.230.133.229:/var/www/maloney-server/
+scp -r build/* larrymm@173.230.133.229:/var/www/maloney-family-server/
+# scp -r build/* larrymm@173.230.133.229:/var/www/maloney-server/
 
 echo "Done! Hopefully"
