421117c61130f14987d3ff83f44c5cea6bb74d80
Lawrence Maloney <lawrence.maloney@att.net>
2022-06-25 16:38:54 -0500

    Initial Commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "Switching  to branch master"
+git checkout master
+
+echo "Building app..."
+npm run build 
+
+echo "Deploying files to server..."
+scp -r build/* larrymm@173.230.133.229:/vsr/www/maloney-server/
+
+echo "Done!"
